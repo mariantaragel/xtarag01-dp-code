9f50d9f1c2008cfe4cbf0a9b86c183a3b81bc8fb
Marián Tarageľ <mariantaragel@gmail.com>
2025-08-25 16:49:35 +0200

    Added base of PC AE

---
+++ models/model_descriptions.py
@@ -0,0 +1,19 @@
+from point_net import PointNet
+from mlp import MLP
+from .pointcloud_autoencoder import PointcloudAutoencoder
+
+def describe_pc_ae(args):
+    # Make an AE.
+    if args.encoder_net == 'pointnet':
+        ae_encoder = PointNet(init_feat_dim=3, conv_dims=args.encoder_conv_layers)
+        encoder_latent_dim = args.encoder_conv_layers[-1]
+    else:
+        raise NotImplementedError()
+
+    if args.decoder_net == 'mlp':
+        ae_decoder = MLP(in_feat_dims=encoder_latent_dim,
+                         out_channels=args.decoder_fc_neurons + [args.n_pc_points * 3],
+                         b_norm=False)
+
+    model = PointcloudAutoencoder(ae_encoder, ae_decoder)
+    return model
